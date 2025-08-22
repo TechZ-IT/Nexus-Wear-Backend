fa846d74cf733f05d458c5b2dac0aab3c2617eda
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-22 14:17:12 +0600

    feat(role): added all endpoints for role

--- src/role/controller/role.controller.ts
+++ src/role/controller/role.controller.ts
@@ -1,4 +1,89 @@
-import { Controller } from '@nestjs/common';
 
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+
+import { CreateRoleDto } from '../dto/create-role.dto';
+import { UpdateRoleDto } from '../dto/update-role.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { RoleService } from '../service/role.service';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
+import { AdminGuard } from 'src/auth/guard/admin.guard';
+import { Role } from '../entity/role.entity';
+
+@ApiTags('role')
+// @UseGuards(JwtAuthGuard, AdminGuard)
 @Controller('role')
-export class RoleController {}
+export class RoleController {
+  constructor(private readonly roleService: RoleService) {}
+
+  @Post()
+  @ApiOperation({ summary: 'Create a new role' })
+  @ApiResponse({
+    status: 201,
+    description: 'The role has been successfully created.',
+    type: Role,
+  })
+  // @UseGuards(PermissionGuard)
+  // @RequirePermission('manage_roles')
+  create(@Body() createRoleDto: CreateRoleDto) {
+    return this.roleService.create(createRoleDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get all role' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all roles',
+    type: [Role],
+  })
+  findAll() {
+    return this.roleService.findAll();
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get a role by ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return the role with the given ID',
+    type: Role,
+  })
+  @ApiResponse({ status: 404, description: 'Role not found' })
+  findOne(@Param('id') id: string) {
+    return this.roleService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update a role' })
+  @ApiResponse({
+    status: 200,
+    description: 'The role has been successfully updated.',
+    type: Role,
+  })
+  @ApiResponse({ status: 404, description: 'Role not found' })
+  // @UseGuards(PermissionGuard)
+  // @RequirePermission('manage_roles')
+  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
+    return this.roleService.update(+id, updateRoleDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete a role' })
+  @ApiResponse({
+    status: 200,
+    description: 'The role has been successfully deleted.',
+  })
+  @ApiResponse({ status: 404, description: 'Role not found' })
+  // @UseGuards(PermissionGuard)
+  // @RequirePermission('manage_roles')
+  remove(@Param('id') id: string) {
+    return this.roleService.remove(+id);
+  }
+}
