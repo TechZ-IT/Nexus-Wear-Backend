ab144d7198c6183b22dc23df99cf3a521f305460
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-22 14:21:22 +0600

    fix(customer): updated customer entity with created at and updated at snake case

--- src/customer/entity/customer.entity.ts
+++ src/customer/entity/customer.entity.ts
@@ -1,5 +1,11 @@
 import { Exclude } from 'class-transformer';
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('customer')
 export class Customer {
@@ -36,4 +42,10 @@ export class Customer {
 
   @Column()
   country: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
