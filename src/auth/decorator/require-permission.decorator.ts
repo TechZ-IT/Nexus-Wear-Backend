c1c706ce353a29c10ba1c3db6611bec3f2e2cfa5
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-22 14:32:39 +0600

    feat(auth): added auth decorators

---
+++ src/auth/decorator/require-permission.decorator.ts
@@ -0,0 +1,6 @@
+// src/permissions/decorators/require-permission.decorator.ts
+import { SetMetadata } from '@nestjs/common';
+
+export const REQUIRED_PERMISSION_KEY = 'requiredPermission';
+export const RequirePermission = (permission: string) => 
+  SetMetadata(REQUIRED_PERMISSION_KEY, permission);
