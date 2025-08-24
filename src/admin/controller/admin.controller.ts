84d0595b04fc6f03cd508ab5ecd3dfda3b3ad15e
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-24 22:27:10 +0600

    feat(admin): added admin create endpoint

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -1,9 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AdminService } from '../service/admin.service';
+import { ApiConsumes } from '@nestjs/swagger';
+import {
+  FileFieldsInterceptor,
+  FileInterceptor,
+} from '@nestjs/platform-express';
+import { CreateAdminDto } from '../dto/create-admin.dto';
 
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileFieldsInterceptor([{ name: 'image', maxCount: 1 }]))
+  @HttpCode(HttpStatus.CREATED)
+  Create(
+    @Body() createAdminDto: CreateAdminDto,
+    @UploadedFiles() files: { image: Express.Multer.File },
+  ) {
+    return this.adminService.create(createAdminDto, files.image?.[0]);
+  }
 }
