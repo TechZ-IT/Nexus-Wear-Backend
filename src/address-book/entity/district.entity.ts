65b4ba9be168fa272ed47d399853517d8b74f04e
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-29 14:43:48 +0600

    feat(address-book): created district entity

---
+++ src/address-book/entity/district.entity.ts
@@ -0,0 +1,12 @@
+import { BaseEntity } from 'src/common/entities/Base.entity';
+import { Column, Entity, OneToMany } from 'typeorm';
+import { SubDistrict } from './sub-district.entity';
+
+@Entity('district')
+export class District extends BaseEntity {
+  @Column()
+  name: string;
+
+  @OneToMany(() => SubDistrict, (subDistrict) => subDistrict.district)
+  subDistrict: SubDistrict[];
+}
