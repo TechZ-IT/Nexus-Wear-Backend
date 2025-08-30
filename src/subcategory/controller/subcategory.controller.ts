775ea4c2d24cf657c494548b762c3ed60d97bbd2
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-30 11:44:56 +0600

    feat(subcategory): added subcategory create endpoint

--- src/subcategory/controller/subcategory.controller.ts
+++ src/subcategory/controller/subcategory.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { SubcategoryService } from '../service/subcategory.service';
+import { ApiConsumes } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateSubcategoryDto } from '../dto/create-subcategory.dto';
 
 @Controller('subcategory')
-export class SubcategoryController {}
+export class SubcategoryController {
+  constructor(private readonly subcategoryService: SubcategoryService) {}
+
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('image'))
+  create(
+    @Body() dto: CreateSubcategoryDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.subcategoryService.create(dto, image);
+  }
+}
