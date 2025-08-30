8ceed3e9c845c7d7e91bcaae02234852b26c19c0
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-30 08:58:32 +0600

    feat(category): Updated category module with imports

--- src/category/module/category.module.ts
+++ src/category/module/category.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from '../controller/category.controller';
 import { CategoryService } from '../service/category.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from '../entity/category.entity';
+import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category]), R2UploadModule],
   controllers: [CategoryController],
-  providers: [CategoryService]
+  providers: [CategoryService],
 })
 export class CategoryModule {}
