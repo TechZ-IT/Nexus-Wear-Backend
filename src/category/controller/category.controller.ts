e39280659d5b4a2e8cb18440fcf036378863bead
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-30 08:59:14 +0600

    feat(category): added category create endpoint

--- src/category/controller/category.controller.ts
+++ src/category/controller/category.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { CategoryService } from '../service/category.service';
+import { R2UploadService } from 'src/r2-upload/service/r2-upload.service';
+import { CreateCategoryDto } from '../dto/create-category.dto';
+import { ApiConsumes } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('category')
-export class CategoryController {}
+export class CategoryController {
+  constructor(
+    private readonly categoryService: CategoryService,
+  ) {}
+
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('image'))
+  create(
+    @Body() dto: CreateCategoryDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.categoryService.create(dto, image)
+  }
+}
