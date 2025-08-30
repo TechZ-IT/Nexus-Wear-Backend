13afbe35ca5c9d2569a2c1c7522c83b0ea2ca05a
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-30 10:12:26 +0600

    fix(subcategory): updated subcategory module with imports

--- src/subcategory/module/subcategory.module.ts
+++ src/subcategory/module/subcategory.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SubcategoryService } from '../service/subcategory.service';
 import { SubcategoryController } from '../controller/subcategory.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Subcategory } from '../entity/subcategory.entity';
+import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Subcategory]), R2UploadModule],
   providers: [SubcategoryService],
-  controllers: [SubcategoryController]
+  controllers: [SubcategoryController],
 })
 export class SubcategoryModule {}
