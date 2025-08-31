051303e0629c7dae3c136189c05dcc0c889014b3
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-08-31 11:08:11 +0600

    fix(customer): fixed customer module with imports

--- src/customer/module/customer.module.ts
+++ src/customer/module/customer.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Customer } from '../entity/customer.entity';
 import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
 import { AuthModule } from 'src/auth/module/auth.module';
+import { Role } from 'src/role/entity/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer]), R2UploadModule, AuthModule],
+  imports: [TypeOrmModule.forFeature([Customer,Role]), R2UploadModule, AuthModule],
   providers: [CustomerService],
   controllers: [CustomerController],
 })
