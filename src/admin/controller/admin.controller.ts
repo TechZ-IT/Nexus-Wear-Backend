0ffd384744df7a449987d97898379ee9fed1afc6
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-03 22:48:50 +0600

    fix(admin): added parse int pipe in admin get all endpoint

--- src/admin/controller/admin.controller.ts
+++ src/admin/controller/admin.controller.ts
@@ -6,6 +6,7 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -54,8 +55,8 @@ export class AdminController {
   @ApiQuery({ name: 'limit', required: false, type: Number })
   @ApiQuery({ name: 'status', required: false, type: String })
   findAll(
-    @Query('page') page = 1,
-    @Query('limit') limit = 10,
+    @Query('page', new ParseIntPipe({ optional: true })) page = 1,
+    @Query('limit', new ParseIntPipe({ optional: true })) limit = 10,
     @Query('status') status?: AdminStatus,
   ) {
     return this.adminService.findAll({ page, limit, status });
