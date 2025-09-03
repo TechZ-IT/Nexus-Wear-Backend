b895b5b1b6cad0e5dbfb5af079f451f92310ed0a
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-04 00:12:31 +0600

    init(color): initiated color service

--- src/color/service/color.service.ts
+++ src/color/service/color.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Color } from '../entity/color.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ColorService {}
+export class ColorService {
+  constructor(
+    @InjectRepository(Color)
+    private readonly colorRepository: Repository<Color>,
+  ) {}
+
+}
