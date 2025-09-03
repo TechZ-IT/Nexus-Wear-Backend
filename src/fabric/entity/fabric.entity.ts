3ff590d30c870e42efda93cd1ca87d63523c8e77
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-03 22:11:25 +0600

    feat(fabric): created fabric entity

--- src/fabric/entity/fabric.entity.ts
+++ src/fabric/entity/fabric.entity.ts
@@ -1,5 +1,14 @@
 import { BaseEntity } from 'src/common/entities/Base.entity';
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 
 @Entity('fabric')
-export class Fabric extends BaseEntity {}
+export class Fabric extends BaseEntity {
+      @Column()
+      name: string;
+    
+      @Column({ nullable: true })
+      description?: string;
+    
+      @Column({ nullable: true })
+      image?: string;
+}
