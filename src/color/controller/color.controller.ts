f5b5150f1160fedc3afacc73a6c488acd927fdff
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-05 20:01:08 +0600

    feat(color): added craete color endpoint

--- src/color/controller/color.controller.ts
+++ src/color/controller/color.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { ColorService } from '../service/color.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateColorDto } from '../dto/create-color.dto';
+import { ApiConsumes } from '@nestjs/swagger';
 
 @Controller('color')
-export class ColorController {}
+export class ColorController {
+  constructor(private readonly colorService: ColorService) {}
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('image'))
+  create(
+    @Body() createColorDto: CreateColorDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.colorService.create(createColorDto, image);
+  }
+}
