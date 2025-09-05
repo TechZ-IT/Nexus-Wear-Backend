35b4569cc5b72d28e76af45989f34271fe3636b3
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-05 20:00:49 +0600

    feat(color): added create color method

--- src/color/service/color.service.ts
+++ src/color/service/color.service.ts
@@ -2,12 +2,39 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Color } from '../entity/color.entity';
 import { Repository } from 'typeorm';
+import { CreateColorDto } from '../dto/create-color.dto';
+import { R2UploadService } from 'src/r2-upload/service/r2-upload.service';
 
 @Injectable()
 export class ColorService {
   constructor(
     @InjectRepository(Color)
     private readonly colorRepository: Repository<Color>,
+
+    private readonly r2UploadService: R2UploadService,
   ) {}
 
+  async create(createColorDto: CreateColorDto, image: Express.Multer.File) {
+    const { image: img, ...withoutImage } = createColorDto;
+
+    const color = this.colorRepository.create(withoutImage);
+    const savedColor = await this.colorRepository.save(color);
+    if (image) {
+      const imageUrl = await this.r2UploadService.uploadImage(
+        image,
+        savedColor.id,
+        'color',
+      );
+      if (!imageUrl) {
+        throw new Error('Color image upload failed');
+      }
+      savedColor.image = imageUrl;
+      await this.colorRepository.save(savedColor);
+    }
+    return {
+      data: savedColor,
+      message: 'Created Color Successfully',
+      status: 'success',
+    };
+  }
 }
