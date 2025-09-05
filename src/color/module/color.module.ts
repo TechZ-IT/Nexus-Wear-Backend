1485827062163719d57aeef4c67c8d70b6cfa1f0
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-05 20:00:24 +0600

    fix(color): updated color module imports

--- src/color/module/color.module.ts
+++ src/color/module/color.module.ts
@@ -3,9 +3,10 @@ import { ColorService } from '../service/color.service';
 import { ColorController } from '../controller/color.controller';
 import { Color } from '../entity/color.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Color])],
+  imports: [TypeOrmModule.forFeature([Color]), R2UploadModule],
   providers: [ColorService],
   controllers: [ColorController],
 })
