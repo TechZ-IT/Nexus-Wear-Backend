f29c5eea270bdd4730ca97da108d6088c8007eaa
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-06 00:59:02 +0600

    fix(color): fixed color upate dto

--- src/color/dto/update-color.dto.ts
+++ src/color/dto/update-color.dto.ts
@@ -1,21 +1,27 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsOptional, IsString } from 'class-validator';
 
 export class UpdateColorDto {
   @ApiPropertyOptional({
     example: 'Red',
     description: 'color name should be here',
   })
+  @IsOptional()
+  @IsString()
   name: string;
 
   @ApiPropertyOptional({
     example: 'here is the red color',
     description: 'color description should be here',
   })
+  @IsOptional()
+  @IsString()
   description: string;
 
   @ApiPropertyOptional({
     type: 'string',
     format: 'binary',
   })
+  @IsOptional()
   image?: Express.Multer.File;
 }
