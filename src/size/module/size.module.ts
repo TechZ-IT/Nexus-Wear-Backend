0327420da72324beb1bd04fc9ac4473f532d6618
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-06 21:18:03 +0600

    feat(size): added size create, update, find all, find one methods

--- src/size/module/size.module.ts
+++ src/size/module/size.module.ts
@@ -3,10 +3,11 @@ import { SizeService } from '../service/size.service';
 import { SizeController } from '../controller/size.controller';
 import { Size } from '../entity/size.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Size])],
+  imports: [TypeOrmModule.forFeature([Size]), R2UploadModule],
   providers: [SizeService],
-  controllers: [SizeController]
+  controllers: [SizeController],
 })
 export class SizeModule {}
