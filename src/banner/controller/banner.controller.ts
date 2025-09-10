baa351eaa817b08451396183543f765552a13e38
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-10 23:42:11 +0600

    feat(banner): banner create endpoint added

--- src/banner/controller/banner.controller.ts
+++ src/banner/controller/banner.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { BannerService } from '../service/banner.service';
+import { ApiConsumes } from '@nestjs/swagger';
+import { CreateBannerDto } from '../dto/create-banner.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('banner')
-export class BannerController {}
+export class BannerController {
+  constructor(private readonly bannerService: BannerService) {}
+
+  @Post()
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('image'))
+  create(
+    @Body() createBannerDto: CreateBannerDto,
+    @UploadedFile() image: Express.Multer.File,
+  ) {
+    return this.bannerService.create(createBannerDto, image);
+  }
+}
