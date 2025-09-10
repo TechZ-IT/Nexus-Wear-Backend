ed3775fcf3539dc143f68bd087d8e7547bda89b9
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-10 23:41:50 +0600

    feat(banner): banner create method added

--- src/banner/module/banner.module.ts
+++ src/banner/module/banner.module.ts
@@ -3,9 +3,11 @@ import { BannerService } from '../service/banner.service';
 import { BannerController } from '../controller/banner.controller';
 import { Banner } from '../entity/banner.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
+import { AuthModule } from 'src/auth/module/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Banner])],
+  imports: [TypeOrmModule.forFeature([Banner]), R2UploadModule, AuthModule],
   providers: [BannerService],
   controllers: [BannerController],
 })
