9c8fa732b7febe4ced6c3d3132b9360f9664b0af
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-09-24 00:15:51 +0600

    feat(product): initiatied product entity

--- src/product/module/product.module.ts
+++ src/product/module/product.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from '../service/product.service';
 import { ProductController } from '../controller/product.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from '../entity/product.entity';
 
 @Module({
+  imports:[TypeOrmModule.forFeature([Product])],
   providers: [ProductService],
   controllers: [ProductController]
 })
