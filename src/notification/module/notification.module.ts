75bf3b71412a54d5ad603988441ea7226e92bee9
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-10-20 11:13:19 +0600

    update(notification): updated notification module with entity import

--- src/notification/module/notification.module.ts
+++ src/notification/module/notification.module.ts
@@ -3,9 +3,15 @@ import { NotificationController } from '../controller/notification.controller';
 import { NotificationService } from '../service/notification.service';
 import { Notification } from '../entity/notification.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { NotificationSeen } from '../entity/notification-seen.entity';
+import { Role } from 'src/role/entity/role.entity';
+import { AuthModule } from 'src/auth/module/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Notification])],
+  imports: [
+    TypeOrmModule.forFeature([Notification, NotificationSeen, Role]),
+    AuthModule,
+  ],
   controllers: [NotificationController],
   providers: [NotificationService],
 })
