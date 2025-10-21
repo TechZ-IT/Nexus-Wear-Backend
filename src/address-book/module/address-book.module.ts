73dbcbbcb301d2740424a4eed472a2d83125e041
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-10-21 21:04:29 +0600

    fix(address-book): updated address book entity

--- src/address-book/module/address-book.module.ts
+++ src/address-book/module/address-book.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AddressBookService } from '../service/address-book.service';
 import { AddressBookController } from '../controller/address-book.controller';
 import { District } from '../entity/district.entity';
-import { SubDistrict } from '../entity/subdistrict';
+import { SubDistrict } from '../entity/subdistrict.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
