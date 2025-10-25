eb499fea4bb550fd85d66e8958d009415f85e31a
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-10-25 18:20:00 +0600

    updated order and payment module

--- src/order/module/order.module.ts
+++ src/order/module/order.module.ts
@@ -3,9 +3,11 @@ import { OrderService } from '../service/order.service';
 import { OrderController } from '../controller/order.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from '../entity/order.entity';
+import { Cart } from 'src/cart/entity/cart.entity';
+import { CartItem } from 'src/cart/entity/cart-item.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [TypeOrmModule.forFeature([Order, Cart, CartItem])],
   providers: [OrderService],
   controllers: [OrderController],
 })
--- src/payment/module/payment.module.ts
+++ src/payment/module/payment.module.ts
@@ -3,9 +3,10 @@ import { PaymentService } from '../service/payment.service';
 import { PaymentController } from '../controller/payment.controller';
 import { Payment } from '../entity/payment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from 'src/order/entity/order.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Payment])],
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   providers: [PaymentService],
   controllers: [PaymentController],
 })
