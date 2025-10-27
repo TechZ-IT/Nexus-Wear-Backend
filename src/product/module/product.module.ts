e6b7b743872f2111079c879572fbbd7c8bac7f27
mdtanbirhosen <mdtanbirhosen912@gmail.com>
2025-10-27 17:58:43 +0600

    feat: imported size and color entity here

--- src/product/module/product.module.ts
+++ src/product/module/product.module.ts
@@ -3,10 +3,15 @@ import { ProductService } from '../service/product.service';
 import { ProductController } from '../controller/product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from '../entity/product.entity';
-import { R2UploadModule } from 'src/r2-upload/module/r2-upload.module';
+import { Category } from 'src/category/entity/category.entity';
+import { Subcategory } from 'src/subcategory/entity/subcategory.entity';
+import { Color } from 'src/color/entity/color.entity';
+import { Size } from 'src/size/entity/size.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), R2UploadModule],
+  imports: [
+    TypeOrmModule.forFeature([Product, Category, Subcategory, Color, Size]),
+  ],
   providers: [ProductService],
   controllers: [ProductController],
 })
